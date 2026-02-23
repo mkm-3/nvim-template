fe078ce1cad9bfe91ad7d95180364f68a5a9c1f2
tchi0000 <tchi0017@gmail.com>
2026-02-23 23:23:01 +0900

    feat: Restartコマンドを追加

--- init.lua
+++ init.lua
@@ -96,6 +96,8 @@ require('lazy').setup({
   },
 })
 
+-- user scripts
+require 'usr.scripts.restart_nvim'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

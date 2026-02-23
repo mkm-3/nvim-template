ec3ef4603759ba614b927c6033062dbfee08f3c3
tchi0000 <tchi0017@gmail.com>
2026-02-24 00:02:33 +0900

    feat: copilotを黙らせる

--- lua/usr/plugins/copilot.lua
+++ lua/usr/plugins/copilot.lua
@@ -6,7 +6,7 @@ return {
     require('copilot').setup {
       suggestion = {
         enabled = true,
-        auto_trigger = true,
+        -- auto_trigger = true,
         keymap = {
           accept_and_goto = '<leader>p',
           accept = '<C-y>',
